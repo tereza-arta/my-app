ea29589cc9b8c92503dcf7c0a4b8bb168863bd45
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-02-28 09:46:22 +0400

    Update file.cpp

--- app/file.cpp
+++ app/file.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <time.h>
 #include <unistd.h>
 
 int main() {
-  std::cout << "Message from cpp-file" << std::endl;
-  std::cout << "Wait for 10 seconds\n";
+  std::cout << "Message from cpp_file:" << std::endl;
+
+  time_t system_time = time(0);
+  std::string date_time = ctime(&system_time);
+
+  std::cout << "Current day and time:\t" << date_time << "\n";
+  std::cout << "Wait for 10 seconds...\n";
   sleep(10);
-  std::cout << "Exit\n";
+  std::cout << "Exit from cpp_file\n";
 
   return 0;
 }
