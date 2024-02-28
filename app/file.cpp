54365179581660e3437cea605dd9e9835eae41f3
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-02-28 09:02:25 +0400

    Update file.cpp

--- app/file.cpp
+++ app/file.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <iostream>
 
 int main() {
-  std::cout < "Message from cpp-file" << std::endl;
+  std::cout << "Message from cpp-file" << std::endl;
+  std::cout << "Wait for 10 seconds\n";
+  sleep(10);
+  std::cout << "Exit\n";
 
   return 0;
 }
